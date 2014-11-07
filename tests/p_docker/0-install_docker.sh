ee279cbaf08a5a268b861a46b23b467d1916f059
Athmane Madjoudj <athmane@fedoraproject.org>
2014-11-07 22:15:29 +0100

    Remove manual install since EPEL is included in extra

--- tests/p_docker/0-install_docker.sh
+++ tests/p_docker/0-install_docker.sh
@@ -9,14 +9,8 @@ t_InstallPackage  wget openssh-clients
 
 if [ "$centos_ver" = "6" ] ; then
 
-	EPEL6_RELEASE_URL="http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm"
-	EPEL6_GPGKEY_URL="https://fedoraproject.org/static/0608B895.txt"
-
-	t_Log "Installing EPEL and some tools ..."
-	cd /root 
-	wget  "$EPEL6_RELEASE_URL"
-	rpm --import  "$EPEL6_GPGKEY_URL"
-	yum -y localinstall /root/epel-release*.rpm
+	t_Log "Enabling EPEL  ..."
+    t_InstallPackage epel-release
 
 else
 	t_Log "CentOS 7: Docker is included in Extra repo"
